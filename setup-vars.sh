5ad568ca301a03786c2ce6a61a7395f7641506bd
Shehan Dondeenu <shehan.au@gmail.com>
2026-05-06 01:43:05 +1000

    Create setup-vars.sh

---
+++ setup-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export ResourceGroup="rg_eastus_343879_1_177798972665"
+export Location="eastus"
+export AdminUser="adminuser"
+export AdminPass="Admin!1234admin" # In production, use a Key Vault!
+export VMSize="Standard_B2s"
